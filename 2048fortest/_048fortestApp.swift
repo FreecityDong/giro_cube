905e8216bc5d82bc021e0b50602bbb9b62e7d94b
FreecityDong <1749697705@qq.com>
2026-01-07 14:30:31 +0800

    Initial Commit

---
+++ 2048fortest/_048fortestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _048fortestApp.swift
+//  2048fortest
+//
+//  Created by hahadong on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct _048fortestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 2048fortestTests/_048fortestTests.swift
@@ -0,0 +1,17 @@
+//
+//  _048fortestTests.swift
+//  2048fortestTests
+//
+//  Created by hahadong on 2026/1/7.
+//
+
+import Testing
+@testable import _048fortest
+
+struct _048fortestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
